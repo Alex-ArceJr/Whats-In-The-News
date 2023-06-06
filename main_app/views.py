a18fdadb9adcac0358d14068537ef6d816de3ef0
Alex-Arce <arcealejandrino@gmail.com>
2023-06-06 15:34:00 -0700

    wip: first api call to work

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+import requests
+API_KEY = '6cb28f21ae384e73a91e188613a694ad'
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def sports(request):
+    url = f'https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles']
+
+    return render(request, 'sports.html', {
+        'articles': articles
+    })
